d97a07a68627e3e3da77fa8b9957958c75676224
Dérick Eduardo <dreduardo2222@gmail.com>
2025-06-06 17:25:41 -0400

    Implementa o modelo da página individual dos parques.

--- parques/base/urls.py
+++ parques/base/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 
-from parques.base.views import base
+from parques.base.views import base, parque_individual
 
 app_name = "base"
 urlpatterns = [
     path("", base, name="base"),
+    path("parque-individual/", parque_individual, name="parque_individual"),
 ]
--- parques/base/views.py
+++ parques/base/views.py
@@ -3,3 +3,7 @@ from django.shortcuts import render
 
 def base(request):
     return render(request, "base/home.html")
+
+
+def parque_individual(request):
+    return render(request, 'base/index.html')
