f129a8c792a35a6177256b477d1c16fd621e2bcb
Dérick Eduardo <dreduardo2222@gmail.com>
2025-06-08 22:59:44 -0400

    Alterações apenas para exibição para o cliente.

--- parques/base/urls.py
+++ parques/base/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 
-from parques.base.views import base, parque_individual
+from parques.base.views import base, parque_layout_1, parque_layout_2
 
 app_name = "base"
 urlpatterns = [
     path("", base, name="base"),
-    path("parque-individual/", parque_individual, name="parque_individual"),
+    path("parque_layout_1/", parque_layout_1, name="parque_layout_1"),
+    path("parque_layout_2/", parque_layout_2, name="parque_layout_2"),
 ]
--- parques/base/views.py
+++ parques/base/views.py
@@ -5,5 +5,9 @@ def base(request):
     return render(request, "base/home.html")
 
 
-def parque_individual(request):
+def parque_layout_1(request):
+    return render(request, 'base/layout_1.html')
+
+
+def parque_layout_2(request):
     return render(request, 'base/index.html')
